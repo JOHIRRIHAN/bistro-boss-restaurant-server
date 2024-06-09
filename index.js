05e836054421caf81df75247837f23069df1a159
JOHIR RIHAN <jr7718301@gmail.com>
2024-06-09 20:40:46 +0600

    Make User Admin And Install JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId,  } = require("mongodb");
 var cors = require("cors");
+var jwt = require('jsonwebtoken');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
